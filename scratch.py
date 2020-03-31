6f39d6ca5ed91b58d8283d5212efdc812d8eea8a
motiwari <mohittiwarinyc+github@gmail.com>
2020-03-31 16:25:02 -0700

    Obtained data, visualizing results in pyprof2calltree working

--- scratch.py
+++ scratch.py
@@ -7,18 +7,18 @@ from data_utils import *
 # imgs = total_images[np.random.choice(range(len(total_images)), size = sample_size, replace = False)]
 # estimate_sigma(imgs)
 
-naive_dcs = np.array([1476933, 12019267, 75048577, 300000000])
-ucb_dcs = np.array([1837033, 8612167, 32016477])
+naive_dcs = np.array([1470000, 12000000, 75000000, 300000000])
+ucb_dcs = np.array([1830100, 8592900, 31967900])
 
-naive_times = np.array([14.201, 122.213, 122.213])
-ucb_times = np.array([22.277, 101.315, 101.315])
+# naive_times = np.array([14.201, 122.213, 122.213])
+# ucb_times = np.array([22.277, 101.315, 101.315])
 
-x = np.arange(500, 5100, 100)
+x = np.arange(500, 10100, 100)
 quadratic =  3*(x**2)
 knlogn = 2.5e2*3*(x*np.log(x))
 
-plt.plot([700, 2000, 5000], naive_dcs, 'bo')
-plt.plot([700, 2000, 5000], ucb_dcs, 'ro')
+plt.plot([700, 2000, 5000, 10000], naive_dcs, 'bo')
+plt.plot([700, 2000, 5000, 10000], ucb_dcs, 'ro')
 plt.plot(x, quadratic, 'b-')
 plt.plot(x, knlogn, 'r-')
 plt.show()
