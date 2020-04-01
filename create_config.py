984666c6f385e140a06709014cf0ceba91341dfd
motiwari <mohittiwarinyc+github@gmail.com>
2020-03-31 21:03:58 -0700

    Removing n = 20 case because batch size is fixed at 100 and cannot sample without replacement

--- create_config.py
+++ create_config.py
@@ -1,6 +1,6 @@
 import itertools
 
-ns = [20, 100, 500, 2000, 5000, 10000, 30000, 70000]
+ns = [100, 500, 2000, 5000, 10000, 30000, 70000]
 ks = [1, 3, 10, 50, 100]
 
 for idx,elem in enumerate(itertools.product(ns, ks)):
