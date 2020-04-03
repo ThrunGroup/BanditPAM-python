e02989cf3d48d2ccfb2addc1ea8b82948f814ac3
motiwari <mohittiwarinyc+github@gmail.com>
2020-04-02 17:07:09 -0700

    Deleting unnecessary files, updating experiments

--- create_config.py
+++ create_config.py
@@ -1,7 +1,8 @@
 import itertools
 
 ns = [100, 500, 2000, 5000, 10000, 30000, 70000]
-ks = [1, 3, 10, 50, 100]
+ks = [1, 3, 10, 50]
 
 for idx,elem in enumerate(itertools.product(ns, ks)):
-  print(str(idx) + " : ['ucb', 0, " + str(elem[1]) + ", " + str(elem[0]) + ", 42, 'MNIST'],")
+    print(str(2*idx) + " : ['naive', 0, " + str(elem[1]) + ", " + str(elem[0]) + ", 42, 'MNIST'],")
+    print(str(2*idx + 1) + " : ['ucb', 0, " + str(elem[1]) + ", " + str(elem[0]) + ", 42, 'MNIST'],")
