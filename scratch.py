a2653cff37326bcde3c0a7a462b1f3e1ad7756be
motiwari <mohittiwarinyc+github@gmail.com>
2020-04-15 11:30:49 -0700

    Adding code to make plots. Dependences on N look quadratic and nlogn, and dependence on k looks linear -- might need to collect k = 20, N = 3000 data for UCB though. Now checking swap steps

--- scratch.py
+++ scratch.py
@@ -51,20 +51,64 @@ from data_utils import *
 
 
 #### for k = 10, build only
-x = [100, 300, 1000, 3000]
+# x = [100, 300, 1000, 3000]
+# xticks = np.arange(100, 3100, 100)
+#
+# naive_B_calls = np.array([107759, 922363, 10079346, 90236487])
+# ucb_B_calls = np.array([167319, 1330223, 15952226, 95086987])
+#
+# plt.plot(x, np.log(naive_B_calls), 'bo')
+# plt.plot(x, np.log(ucb_B_calls), 'ro')
+# quadratic = (3*xticks)**2
+# nlogn = (xticks*np.log(xticks))
+#
+# plt.plot(xticks, np.log(quadratic), 'b-')
+# plt.plot(xticks, np.log(nlogn), 'r-')
+# plt.title("Number of distance calls for k = 10 medoids")
+# plt.xlabel("N")
+# plt.ylabel("Calls to d")
+# plt.show()
+
+
+
+# Build only, k = 3, N = 300, 1000, 3000, 10000
+x = [300, 1000, 3000, 10000, 30000]
 xticks = np.arange(100, 3100, 100)
 
-naive_B_calls = np.array([107759, 922363, 10079346, 90236487])
-ucb_B_calls = np.array([167319, 1330223, 15952226, 95086987])
+ucb_B_calls = np.array([230455, 2124463, 7012741, 34433219, 112562578])
+naive_B_calls = np.array([273035, 3011023, 27032741])
 
-plt.plot(x, np.log(naive_B_calls), 'bo')
-plt.plot(x, np.log(ucb_B_calls), 'ro')
-quadratic = (3*xticks)**2
-nlogn = (xticks*np.log(xticks))
 
-plt.plot(xticks, np.log(quadratic), 'b-')
-plt.plot(xticks, np.log(nlogn), 'r-')
-plt.title("Number of distance calls for k = 10 medoids")
+plt.plot(x[:-2], naive_B_calls, 'bo')
+plt.plot(x[:-2], ucb_B_calls[:-2], 'ro')
+quadratic = (3e-1*xticks)**2
+quadratic_naive = (1.7*xticks)**2
+nlogn = (4e2*xticks*np.log(xticks))
+
+plt.plot(xticks, quadratic, 'g-')
+plt.plot(xticks, quadratic_naive, 'b-')
+plt.plot(xticks, nlogn, 'r-')
+plt.title("Number of distance calls for k = 3 medoids vs N, BUILD only")
 plt.xlabel("N")
 plt.ylabel("Calls to d")
 plt.show()
+
+
+# Build only, scaling with k: 1, 2, 3 and N = 3000
+k = [1, 2, 3, 4, 5, 10]
+
+naive_B_calls = np.array([9006000, 18017194, 27032741, 36051842, 45074455, 90236487])
+ucb_B_calls = np.array([284820, 4345814, 7012741, 12470922, 17469475, 95086987])
+
+linear_naive = 9e6*np.array(k)
+linear_ucb = 3.5e6*(np.array(k))
+plt.plot(k, naive_B_calls, 'bo')
+plt.plot(k, ucb_B_calls, 'ro')
+plt.plot(k, linear_naive)
+plt.plot(k, linear_ucb)
+plt.show()
+
+# Build and Swap, k = 3, N = 1000, 3000, 10000
+
+
+# Build and Swap, scaling with k: 1, 2, 3 and N = 3000
