1f3f2f3e6e900d33e51aee144df9841821e5042c
motiwari <mohittiwarinyc+github@gmail.com>
2020-05-12 14:30:22 -0700

    Adding profiles for (SCRNA, L1) and (SCRNA, L2) and experiment logs for the compute_exactly. Note that (SCRNA, L1) is still in progress

--- sc_RNA_exp_config.py
+++
@@ -1,9 +0,0 @@
-experiments = [
-    # Exp number : MUST respect same order of argparse
-    # script, verbosity, num_medoids, sample_size, seed, dataset
-
-    ['ucb', 'B', 0, 5, 40000, 42, 'SCRNA', ''],
-    ['ucb', 'BS', 0, 5, 40000, 42, 'SCRNA', ''],
-    ['naive', 'B', 0, 5, 40000, 42, 'SCRNA', ''],
-    ['naive', 'BS', 0, 5, 40000, 42, 'SCRNA', ''],
-]
