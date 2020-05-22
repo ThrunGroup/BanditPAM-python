c0d6c6cbbde8e9b281095adc778b5d651ec0039b
motiwari <mohittiwarinyc+github@gmail.com>
2020-05-22 00:58:44 -0700

    Added code to automatically make graphs. Very messy but working

--- parse_profiles.py
+++ parse_profiles.py
@@ -1,8 +1,12 @@
 import sys
 import os
 import pstats
+import numpy as np
+import matplotlib.pyplot as plt
 
 import snakevizcode
+
+from generate_config import write_exp
 
 FN_NAME = 'data_utils.py:104(empty_counter)'
 
@@ -25,17 +29,48 @@ def main():
     dataset = 'MNIST'
     metric = 'L2'
 
-    Ns = [1000, 3000, 10000, 30000, 70000]
-    ks = [2, 3, 4, 5, 10, 20, 30]
-    seeds = range(10)
-    
-    profiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store' and x[:2] == 'p-']
-    for profile in profiles:
-        p = pstats.Stats(profile)
-        for row in snakevizcode.table_rows(p):
-            if FN_NAME in row:
-                d_calls = row[0][1]
-                print(profile, d_calls)
+    Ns = [1000, 3000, 10000]#, 30000, 70000]
+    ks = [2]#, 3, 4, 5, 10, 20, 30]
+    seeds = range(1)
+
+    build_profiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store' and x[:2] == 'p-B']
+    swap_profiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store' and x[:2] == 'p-S']
+
+    ucb_build = np.zeros((len(ks), len(Ns), len(seeds)))
+    ucb_swap = np.zeros((len(ks), len(Ns), len(seeds)))
+
+    naive_build = np.zeros((len(ks), len(Ns), len(seeds)))
+    naive_swap = np.zeros((len(ks), len(Ns), len(seeds)))
+
+    for algo in algos:
+        for N_idx, N in enumerate(Ns):
+            for k_idx, k in enumerate(ks):
+                for seed_idx, seed in enumerate(seeds):
+                    # WARNING: Brittle
+                    # WARNING: Remove the +42!!!!
+                    build_profile = 'profiles/p-B-' + algo + '-True-BS-v-0-k-' + str(k) + \
+                        '-N-' + str(N) + '-s-' + str(seed + 42) + '-d-' + dataset + '-m-' + metric + '-w-'
+                    if os.path.exists(build_profile):
+                        p = pstats.Stats(build_profile)
+                        for row in snakevizcode.table_rows(p):
+                            if FN_NAME in row:
+                                d_calls = row[0][1]
+                                if algo == 'ucb':
+                                    ucb_build[k_idx][N_idx][seed_idx] = d_calls
+                                elif algo == 'naive_v1':
+                                    naive_build[k_idx][N_idx][seed_idx] = d_calls
+                                else:
+                                    raise Exception("bad algo yo")
+    print(ucb_build)
+    for k_idx, k in enumerate(ks):
+        print(np.mean(ucb_build[k_idx], axis = 1))
+        plt.plot(Ns, np.mean(ucb_build[k_idx], axis = 1))
+        plt.show()
+    print(naive_build)
+    for k_idx, k in enumerate(ks):
+        print(np.mean(naive_build[k_idx], axis = 1))
+        plt.plot(Ns, np.mean(naive_build[k_idx], axis = 1))
+        plt.show()
 
 if __name__ == '__main__':
     verify_logfiles()
