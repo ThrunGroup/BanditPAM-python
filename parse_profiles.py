ef5fe7dc0a29dd8c2aa58003b286876bf93fd198
motiwari <mohittiwarinyc+github@gmail.com>
2020-05-22 14:51:38 -0700

    Able to make plots for build step for both vs k and vs N

--- parse_profiles.py
+++ parse_profiles.py
@@ -10,6 +10,13 @@ from generate_config import write_exp
 
 FN_NAME = 'data_utils.py:104(empty_counter)'
 
+def showx():
+    plt.draw()
+    plt.pause(1) # <-------
+    input("<Hit Enter To Close>")
+    plt.close()
+
+
 def verify_logfiles():
     ucb_logfiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store' and x[:5] == 'L-ucb']
     for u_lfile in ucb_logfiles:
@@ -24,53 +31,82 @@ def verify_logfiles():
                             print("ERROR: Results for", u_lfile, "disagree!!")
                     print("OK: Results for", u_lfile, "agree")
 
+def plot_slice(dcalls_array, vs_k_or_N, Ns, ks, algo, seeds):
+    assert vs_k_or_N == 'N' or vs_k_or_N == 'k', "Bad slice param"
+
+    if vs_k_or_N == 'k':
+        kNs = ks
+        Nks = Ns
+    elif vs_k_or_N == 'N':
+        kNs = Ns
+        Nks = ks
+
+    for kN_idx, kN in enumerate(kNs):
+        if vs_k_or_N == 'k':
+            plt.title(algo + " scaling with N for k = " + str(kN))
+        elif vs_k_or_N == 'N':
+            plt.title(algo + " scaling with k for N = " + str(kN))
+        plt.xlabel("N")
+        for seed_idx, seed in enumerate(seeds):
+            if vs_k_or_N == 'k':
+                plt.plot(Nks, dcalls_array[kN_idx, :, seed_idx], 'o')
+            elif vs_k_or_N == 'N':
+                plt.plot(Nks, dcalls_array[:, kN_idx, seed_idx], 'o')
+        if vs_k_or_N == 'k':
+            plt.plot(Nks, np.mean(dcalls_array[kN_idx], axis = 1), 'b-')
+        elif vs_k_or_N == 'N':
+            plt.plot(Nks, np.mean(dcalls_array[:, kN_idx], axis = 1), 'b-')
+        showx()
+
+def show_plots(vs_k_or_N, build_or_swap, Ns, ks, seeds, algos, dataset, metric):
+    dcalls_array = np.zeros((len(ks), len(Ns), len(seeds)))
+
+    if build_or_swap == 'build':
+        prefix = 'profiles/p-B-'
+    elif build_or_swap == 'swap':
+        prefix = 'profiles/p-S-'
+    else:
+        raise Exception("Error pi")
+
+    log_prefix = 'L-'
+
+    # Gather data
+    for algo in algos:
+        assert algo in ['ucb', 'naive_v1'], "Bad algo yo"
+        for N_idx, N in enumerate(Ns):
+            for k_idx, k in enumerate(ks):
+                for seed_idx, seed in enumerate(seeds):
+                    profile_fname = prefix + algo + '-True-BS-v-0-k-' + str(k) + \
+                        '-N-' + str(N) + '-s-' + str(seed) + '-d-' + dataset + '-m-' + metric + '-w-'
+                    if os.path.exists(profile_fname):
+                        p = pstats.Stats(profile_fname)
+                        for row in snakevizcode.table_rows(p):
+                            if FN_NAME in row:
+                                dcalls = row[0][1]
+                                dcalls_array[k_idx][N_idx][seed_idx] = dcalls
+                    else:
+                        print("Warning: profile not found for ", profile_fname)
+
+    # Show data
+    for algo in algos:
+        plot_slice(dcalls_array, 'k', Ns, ks, algo, seeds)
+        plot_slice(dcalls_array, 'N', Ns, ks, algo, seeds)
+
 def main():
-    algos = ['ucb', 'naive_v1']
+    algos = ['ucb']#, 'naive_v1']
     dataset = 'MNIST'
     metric = 'L2'
 
-    Ns = [1000, 3000, 10000]#, 30000, 70000]
-    ks = [2]#, 3, 4, 5, 10, 20, 30]
-    seeds = range(1)
+    Ns = [1000, 3000, 10000, 30000, 70000]
+    ks = [2, 3, 4]#, 5, 10, 20, 30]
+    seeds = range(4)
 
     build_profiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store' and x[:2] == 'p-B']
     swap_profiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store' and x[:2] == 'p-S']
 
-    ucb_build = np.zeros((len(ks), len(Ns), len(seeds)))
-    ucb_swap = np.zeros((len(ks), len(Ns), len(seeds)))
+    show_plots('k', 'build', Ns, ks, seeds, algos, dataset, metric)
 
-    naive_build = np.zeros((len(ks), len(Ns), len(seeds)))
-    naive_swap = np.zeros((len(ks), len(Ns), len(seeds)))
 
-    for algo in algos:
-        for N_idx, N in enumerate(Ns):
-            for k_idx, k in enumerate(ks):
-                for seed_idx, seed in enumerate(seeds):
-                    # WARNING: Brittle
-                    # WARNING: Remove the +42!!!!
-                    build_profile = 'profiles/p-B-' + algo + '-True-BS-v-0-k-' + str(k) + \
-                        '-N-' + str(N) + '-s-' + str(seed + 42) + '-d-' + dataset + '-m-' + metric + '-w-'
-                    if os.path.exists(build_profile):
-                        p = pstats.Stats(build_profile)
-                        for row in snakevizcode.table_rows(p):
-                            if FN_NAME in row:
-                                d_calls = row[0][1]
-                                if algo == 'ucb':
-                                    ucb_build[k_idx][N_idx][seed_idx] = d_calls
-                                elif algo == 'naive_v1':
-                                    naive_build[k_idx][N_idx][seed_idx] = d_calls
-                                else:
-                                    raise Exception("bad algo yo")
-    print(ucb_build)
-    for k_idx, k in enumerate(ks):
-        print(np.mean(ucb_build[k_idx], axis = 1))
-        plt.plot(Ns, np.mean(ucb_build[k_idx], axis = 1))
-        plt.show()
-    print(naive_build)
-    for k_idx, k in enumerate(ks):
-        print(np.mean(naive_build[k_idx], axis = 1))
-        plt.plot(Ns, np.mean(naive_build[k_idx], axis = 1))
-        plt.show()
 
 if __name__ == '__main__':
     verify_logfiles()
