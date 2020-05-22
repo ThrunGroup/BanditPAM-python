c5cd719c722f0bb15d9075e6623181c37222aed0
motiwari <mohittiwarinyc+github@gmail.com>
2020-05-22 00:02:23 -0700

    Mining function calls successfully

--- parse_profiles.py
+++ parse_profiles.py
@@ -2,11 +2,17 @@ import sys
 import os
 import pstats
 
+import snakevizcode
+
+FN_NAME = 'data_utils.py:104(empty_counter)'
+
 def main():
-    profiles = [os.path.join('profiles', x) for x in os.listdir('profiles')]
+    profiles = [os.path.join('profiles', x) for x in os.listdir('profiles') if os.path.isfile(os.path.join('profiles', x)) and x != '.DS_Store']
     for profile in profiles:
         p = pstats.Stats(profile)
-        p.strip_dirs().print_stats('data_utils')
+        for row in snakevizcode.table_rows(p):
+            if FN_NAME in row:
+                print(row[0][1], type(row[0][1]))
 
 if __name__ == '__main__':
     main()
