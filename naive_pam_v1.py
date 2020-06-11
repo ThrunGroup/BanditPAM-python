9d23b86e1ae778cc7859cd9d171b06542d933c32
motiwari <mohittiwarinyc+github@gmail.com>
2020-06-10 22:56:42 -0700

    Updating naive_v1

--- naive_pam_v1.py
+++ naive_pam_v1.py
@@ -1,15 +1,12 @@
 '''
 This is an "optimized" version of PAM, and should be used to certify
-correctness (in a relatively slow way).
+correctness, but is relatively slow since PAM is O(kn^2).
 
 In particular, it contains the following optimizations over naive_pam:
-1. n --> (n-k) [BUILD done, SWAP not gonna do]
+1. n --> (n-k) in the BUILD step (but not in the SWAP step for convenience)
 2. Array broadcasting instead of looping over losses [BUILD done, SWAP done]
-3. FastPAM1 [SWAP done]
-4. FastPAM2 [WIP]
-5. LAB [WIP]
+3. FastPAM1 [done -- only applies to SWAP step]
 '''
-
 
 from data_utils import *
 import itertools
@@ -45,13 +42,13 @@ def naive_build(args, imgs):
         losses = np.zeros(len(target_idcs))
         for t_reidx, t in enumerate(target_idcs):
             refs = np.arange(N)
-            ref_imgs = imgs[refs] # should be == imgs, since sampling all reference points #NOTE: what if we randomly choose subsample? Impt baseline
+            ref_imgs = imgs[refs] # should be == imgs, since sampling all reference points
             if metric == 'TREE':
                 losses[t_reidx] = np.mean( np.minimum(d_tree(imgs[t], ref_imgs, metric = metric), best_distances)).round(DECIMAL_DIGITS)
             else:
                 losses[t_reidx] = np.mean( np.minimum(d(imgs[t].reshape(1, -1), ref_imgs, metric = metric), best_distances)).round(DECIMAL_DIGITS)
 
-        best_loss_reidx = np.where(losses == losses.min())[0][0] # NOTE: what about duplicates? Chooses first I believe
+        best_loss_reidx = np.where(losses == losses.min())[0][0]
         best_medoid = target_idcs[best_loss_reidx]
 
         medoids.append(best_medoid)
@@ -68,23 +65,27 @@ def naive_build(args, imgs):
 
 
 def naive_swap(args, imgs, init_medoids):
+    '''
+    Iteratively swap the medoid-nonmedoid pair that would lower the loss the
+    most, amongst all such possible swaps. Iterated until convergence, i.e. when
+    the total loss can no longer be lowered in such a manner.
+    '''
+
     S_logstring = init_logstring()
     metric = args.metric
     k = len(init_medoids)
     N = len(imgs)
     max_iter = 1e4
-    # NOTE: Right now can compute amongst all k*n arms. Later make this k*(n-k) -- don't consider swapping medoid w medoid
 
     medoids = init_medoids.copy()
     best_distances, closest_medoids, second_best_distances = get_best_distances(medoids, imgs, return_second_best = True, metric = metric)
     loss = np.mean(best_distances)
     iter = 0
     swap_performed = True
-    while swap_performed and iter < max_iter: # not converged
+    while swap_performed and iter < max_iter:
         iter += 1
         new_losses = np.inf * np.ones((k, N))
-
-        swap_candidates = np.array(list(itertools.product(range(k), range(N)))) # A candidate is a PAIR
+        swap_candidates = np.array(list(itertools.product(range(k), range(N))))
 
         if args.fast_pam1:
             new_losses = cost_fn_difference_FP1(imgs, swap_candidates, range(N), medoids, metric = metric).reshape(k, N)
@@ -92,9 +93,8 @@ def naive_swap(args, imgs, init_medoids):
             new_losses = cost_fn_difference(imgs, swap_candidates, range(N), medoids, metric = metric).reshape(k, N)
 
         new_losses = new_losses.round(DECIMAL_DIGITS)
-
         best_swaps = zip( np.where(new_losses == new_losses.min())[0], np.where(new_losses == new_losses.min())[1])
-        best_swaps = list(best_swaps) # NOTE: possible to get first elem of zip object without converting to list?
+        best_swaps = list(best_swaps) # Is it possible to get first elem of zip object without converting to list?
         best_swap = best_swaps[0]
 
         old_medoid_x = medoids[best_swap[0]]
@@ -108,9 +108,13 @@ def naive_swap(args, imgs, init_medoids):
     return medoids, S_logstring, iter, loss
 
 def naive_build_and_swap(args):
+    '''
+    Run the entire PAM algorithm, both the BUILD step and the SWAP step
+    '''
     num_swaps = -1
     final_loss = -1
 
+    # Randomly sample the data
     total_images, total_labels, sigma = load_data(args)
     np.random.seed(args.seed)
     if args.metric == 'TREE':
@@ -118,12 +122,14 @@ def naive_build_and_swap(args):
     else:
         imgs = total_images[np.random.choice(range(len(total_images)), size = args.sample_size, replace = False)]
 
+    # Perform BUILD step
     built_medoids = []
     B_logstring = {}
     if 'B' in args.build_ao_swap:
         built_medoids, B_logstring = naive_build(args, imgs)
         print("Built medoids", built_medoids)
 
+    # Perform SWAP step
     swapped_medoids = []
     S_logstring = {}
     if 'S' in args.build_ao_swap:
